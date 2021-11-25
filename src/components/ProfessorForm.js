acc34c1080caf3730bfc581b20d8fca59a960e4f
mateus_silvano <mateus_silvano@hotmail.com>
2021-11-25 16:52:01 -0300

    Alteração Banco de Dados

--- src/components/ProfessorForm.js
+++ src/components/ProfessorForm.js
@@ -7,8 +7,8 @@ export default class ProfessorForm extends React.Component{
         super (props)
 
         this.state = {
-            codigo: this.props.codigo || "",
-            nome: "",
+            
+            nome: this.props.nome || "",
             email: "",
             senha: "",
             dtNascimento: "",
@@ -32,7 +32,6 @@ export default class ProfessorForm extends React.Component{
     handleSubmit = (event) => {
         event.preventDefault()
         let professor = {
-            codigo: this.state.codigo,
             nome: this.state.nome,
             email: this.state.email,
             senha: this.state.senha,
@@ -48,22 +47,20 @@ export default class ProfessorForm extends React.Component{
     }
 
     render(){
-        return <form onSubmit={this.handleSubmit}>
-            <Box display='flex' flexDirection="column">
-            <TextField type="number" variant="standard" name="codigo" label="Codigo" onChange={this.handleChange} value={this.state.codigo}></TextField>
-            <TextField type="text" variant="standard" name="nome" label="Nome" onChange={this.handleChange} value={this.state.nome}></TextField>
-            <TextField type="text" variant="standard" name="email" label="E-mail" onChange={this.handleChange} value={this.state.email}></TextField>
-            <TextField type="password" variant="standard" name="senha" label="Senha" onChange={this.handleChange} value={this.state.senha}></TextField>
-            <TextField type="number" variant="standard" name="dtNascimento" label="Data de Nascimento" onChange={this.handleChange} value={this.state.dtNascimento}></TextField>
-            <TextField type="number" variant="standard" name="cpf" label="CPF" onChange={this.handleChange} value={this.state.cpf}></TextField>
+        return <form onSubmit={this.handleSubmit} sx={{ m: 1, minWidth: '120' }}>
+            <Box sx={{'& > :not(style)': { m: 1, width: '25ch' },}}/*display='flex' flexDirection="column"*/>
+            <TextField type="text" required variant="standard" name="nome" label="Nome" onChange={this.handleChange} value={this.state.nome}></TextField>
+            <TextField type="text" required variant="standard" name="email" label="E-mail" onChange={this.handleChange} value={this.state.email}></TextField>
+            <TextField type="password" required variant="standard" name="senha" label="Senha" onChange={this.handleChange} value={this.state.senha}></TextField>
+            <TextField type="number" required variant="standard" name="dtNascimento" label="Data de Nascimento" onChange={this.handleChange} value={this.state.dtNascimento}></TextField>
+            <TextField type="number" required variant="standard" name="cpf" label="CPF" onChange={this.handleChange} value={this.state.cpf}></TextField>
             <TextField type="number" variant="standard" name="telefone" label="Telefone" onChange={this.handleChange} value={this.state.telefone}></TextField>
             <TextField type="text" variant="standard" name="nacionalidade" label="Nacionalidade" onChange={this.handleChange} value={this.state.nacionalidade}></TextField>
             <TextField type="text" variant="standard" name="sexo" label="Sexo" onChange={this.handleChange} value={this.state.sexo}></TextField>
-            <TextField type="text" variant="standard" name="função" label="Função" onChange={this.handleChange} value={this.state.função}></TextField>
-            <TextField type="text" variant="standard" name="escolaridade" label="Escolaridade" onChange={this.handleChange} value={this.state.escolaridade}></TextField>
+            <TextField type="text" required variant="standard" name="função" label="Função" onChange={this.handleChange} value={this.state.função}></TextField>
+            <TextField type="text" required variant="standard" name="escolaridade" label="Escolaridade" onChange={this.handleChange} value={this.state.escolaridade}></TextField>
             <Button type="submit" name="confirm" >Confirmar</Button>
             </Box>
         </form>
     }
 }
-
